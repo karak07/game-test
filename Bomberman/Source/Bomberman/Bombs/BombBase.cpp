ddc96fbe689b63862b029e1aefdf9a2a74def859
Piotr Fus <fus@nukklear.com>
2017-12-26 17:57:24 +0100

    -static level with static top down view camera -2 player gameplay both with keybord controls (2 PlayerControllers created, but controls are handled on the first one in the level blueprint - workaround to get 2 keyboard inputs working) -players are able to walk around and place bombs -bombs are exploding after preset time(3s), inflicting damage to walls - damage is applied in linear directions -destructible walls are being destroyed by the bomb (no animation at the time), but are blocking the blast -indestructible walls are blocking the blast and are not destroyed -players are not blocking the blast and are killed if in range (dying/win conditions not handled yet) -bomb explosions are triggering other bombs when they are in blast range -bombs are triggering particle system when exploding
    -bomb class has a base class in C++ and is extended in blueprints
    -one player pawn blueprint class, material colored differently on player spawning
    
    -initial commit, includes starter content

---
+++ Bomberman/Source/Bomberman/Bombs/BombBase.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BombBase.h"
+
+// Sets default values
+ABombBase::ABombBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	ConstructorHelpers::FObjectFinder<UParticleSystem> BombExplosion(TEXT("ParticleSystem'/Game/StarterContent/Particles/P_Explosion.P_Explosion'"));
+	ExplosionParticleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("BombExplosion"));
+
+	if (BombExplosion.Succeeded()) {
+		ExplosionParticleSystem->Template = BombExplosion.Object;
+	}
+	ExplosionParticleSystem->bAutoActivate = false;
+	ExplosionParticleSystem->SetHiddenInGame(false);
+}
+
+USceneComponent* ABombBase::GetStaticMeshComponent()
+{
+	return Cast<USceneComponent>(FindComponentByClass(UStaticMeshComponent::StaticClass()));
+}
+
+// Called when the game starts or when spawned
+void ABombBase::BeginPlay()
+{
+	Super::BeginPlay();
+	ExplosionParticleSystem->AttachToComponent(GetStaticMeshComponent(), FAttachmentTransformRules::KeepWorldTransform);
+}
+
+// Called every frame
+void ABombBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABombBase::Explode_Implementation()
+{
+	ExplosionParticleSystem->ActivateSystem();
+	GetStaticMeshComponent()->SetVisibility(false);
+	SetActorEnableCollision(false);
+
+	FName ExplosionTrace(TEXT("ExplosionTrace"));
+	FCollisionQueryParams ExplosionTraceParams = FCollisionQueryParams(ExplosionTrace, false, this);
+	GetWorld()->DebugDrawTraceTag = ExplosionTrace;
+
+	TArray<FHitResult> Hits;
+	FVector DirVectors[] = { /*Up*/ FVector(1.0f, 0.0f, 0.0f), /*Down*/ FVector(-1.0f, 0.0f, 0.0f), /*Left*/ FVector(0.0f, -1.0f, 0.0f), /*Right*/ FVector(0.0f, 1.0f, 0.0f) };
+	float BombRange = 500.0f;
+
+	for (FVector DirVector : DirVectors)
+	{
+		//call GetWorld() from within an actor extending class
+		GetWorld()->LineTraceMultiByChannel(
+			Hits,				   //results
+			GetActorLocation(),    //start
+			GetActorLocation() + DirVector*BombRange, //end
+			ECC_GameTraceChannel1, //collision channel
+			ExplosionTraceParams
+		);
+
+		for (FHitResult hit : Hits)
+		{
+			AActor* hitActor = hit.GetActor();
+			if (hitActor->IsValidLowLevel())
+			{
+				hit.GetActor()->TakeDamage(1.0f, FDamageEvent(), nullptr, this);
+			}
+		}
+	}
+	ExplosionParticleSystem->OnSystemFinished.AddDynamic(this, &ABombBase::OnFinished);
+}
+
+void ABombBase::OnFinished(UParticleSystemComponent* PSystem)
+{
+	Destroy();
+}
