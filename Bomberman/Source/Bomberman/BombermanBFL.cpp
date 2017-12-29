a287699d9595dbe3ccb70ca85f57b3c1e38a473d
Piotr Fus <fuspiotr@gmail.com>
2017-12-29 03:20:26 +0100

    o Show a map timer, that counts down and ends the round o After round end, freeze game in its current state o Show win screen when only one player is alive o Show draw when the last players die in the same bomb blast (or chained bombs) or multiple players are alive when the timer runs out • Reset option on end screen ~3h

---
+++ Bomberman/Source/Bomberman/BombermanBFL.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BombermanBFL.h"
+
+
+FString UBombermanBFL::FormatAsTime(int timeSeconds)
+{
+	return FString::Printf(TEXT("%02d:%02d"), timeSeconds / 60, timeSeconds % 60);
+}
+
+
+
+
+
---
+++ Bomberman/Source/Bomberman/BombermanBFL.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BombermanBFL.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBERMAN_API UBombermanBFL : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	/** Formats time in seconds from integer to MM:SS string format */
+	UFUNCTION(BlueprintCallable, Category = "StringHelpers")
+	static FString FormatAsTime(int timeSeconds);
+	
+};
